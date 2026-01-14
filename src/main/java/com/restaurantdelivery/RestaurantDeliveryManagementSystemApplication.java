34dce28a5163f37c9d15b3a67f14ee04822b60a2
NishaKadam05 <nishaanandraokadam@gmail.com>
2026-01-14 18:14:36 +0530

    first commit

---
+++ src/main/java/com/restaurantdelivery/RestaurantDeliveryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.restaurantdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantDeliveryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantDeliveryManagementSystemApplication.class, args);
+	}
+
+}
